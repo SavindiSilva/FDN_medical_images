5975a1d7e9b0f0b154dd41be8089e57ba1ba795c
Savindi Silva <savindisilva21@gmail.com>
2025-12-20 11:31:44 +0530

    update models file

--- src/models.py
+++ src/models.py
@@ -1,8 +1,16 @@
+import torch
 import torch.nn as nn
 import timm
 
-def get_model(model_name='resnet50', num_classes=7):
-    print(f"loading model: {model_name}...")
-    #downloads the ResNet structure
-    model = timm.create_model(model_name, pretrained=True, num_classes=num_classes)
+def get_model(model_name='resnet50', num_classes=7, pretrained=True):
+    print(f"building {model_name}")
+    try:
+        #try loading from TIMM
+        model = timm.create_model(model_name, pretrained=pretrained, num_classes=num_classes)
+    except Exception as e:
+        print(f"timm not found/error: {e}. using torchvision.")
+        import torchvision.models as models
+        model = models.resnet50(pretrained=pretrained)
+        model.fc = nn.Linear(model.fc.in_features, num_classes)
+        
     return model
